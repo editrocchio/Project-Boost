2484c884d23b2b8e3e91161607b0c6ed82927bdc
Emilio <editrocchio@gmail.com>
2018-08-15 22:14:33 -0700

    added control input

---
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    private Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput() {
+        if(Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        else if(Input.GetKey(KeyCode.A)) {
+            rigidBody.AddRelativeForce(Vector3.left);
+        }
+        else if(Input.GetKey(KeyCode.D)) {
+            rigidBody.AddRelativeForce(Vector3.right);
+        }
+    }
+}
