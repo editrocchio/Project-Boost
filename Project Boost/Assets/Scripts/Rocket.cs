3d11356b6100aa23a3bb8ae275b7169b29ed75fd
Emilio <editrocchio@gmail.com>
2018-08-17 01:11:17 -0700

    fixed rocket thrust

--- Project Boost/Assets/Scripts/Rocket.cs
+++ Project Boost/Assets/Scripts/Rocket.cs
@@ -7,35 +7,47 @@ public class Rocket : MonoBehaviour {
 
     private Rigidbody rigidBody;
     private AudioSource audioSource;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 3000f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessInput();
-	}
+        Thrust();
+        Rotate();
+    }
 
-    private void ProcessInput() {
-        if(Input.GetKey(KeyCode.Space)) {
-            rigidBody.AddRelativeForce(Vector3.up);
+    private void Thrust() {
+        float mainPerFrame = mainThrust * Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space)) {
+            rigidBody.AddRelativeForce(Vector3.up * mainPerFrame);
             if (!audioSource.isPlaying) {
                 audioSource.Play();
             }
-        }
-        else {
+        } else {
             audioSource.Stop();
         }
-
-        if(Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.forward);
-        }
-
-        else if(Input.GetKey(KeyCode.D)) {
-            transform.Rotate(-Vector3.forward);
-        }
     }
+
+        private void Rotate() {
+
+        rigidBody.freezeRotation = true;
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+
+        else if (Input.GetKey(KeyCode.D)){
+            transform.Rotate(-Vector3.forward *rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false;
+    }
+
 }
